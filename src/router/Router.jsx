4f692fe8e7a8ae7a1f2be683f3c0800206527113
Yejin Choi <llhdf123@naver.com>
2024-06-18 11:18:13 +0900

    🎉 240618 [add]:초기세팅
    다시 초기세팅

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -5,11 +5,11 @@ export default function Router() {
   return (
     <BrowserRouter>
       <Route element={<DefaultLayout />}>
-        <Route path="/" element={<Mainpage />} />
+        {/* <Route path="/" element={<Mainpage />} /> */}
         <Route path="/login" element={<Login />} />
-        <Route path="/join" element={<Join />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/detail/:id" element={<DetailedPost />} />
+        {/* <Route path="/join" element={<Join />} /> */}
+        {/* <Route path="/mypage" element={<MyPage />} /> */}
+        {/* <Route path="/detail/:id" element={<DetailedPost />} /> */}
       </Route>
     </BrowserRouter>
   );
