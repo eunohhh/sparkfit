18cf72a83c3b5d196887e9eb0de515bac9e708e2
kimyong <right4570@naver.com>
2024-06-18 15:15:56 +0900

    fix: sidebar ui 수정 및 tailwindcss 추가함

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,25 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import DefaultLayout from '../layout/DefaultLayout';
-import GatheringList from '../page/GatheringList';
+import GatheringList from '../pages/GatheringList';
 import NavermapScriptComponent from '../pages/MainPage/NavermapScriptComponent';
 import DefaultLayout from '../layout/DefaultLayout';
 import DetailedPost from '../pages/DetailedPost';
 import MyPage from '@/components/myPage/MyPage';
 
-
 export default function Router() {
   return (
     <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route path="/" element={<NavermapScriptComponent />} />
-            <Route path="/gathering" element={<GatheringList />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/join" element={<Join />} />
-            <Route path="/mypage" element={<MyPage />} />
-            <Route path="/detail/:id" element={<DetailedPost />} />
-          </Route>
-        </Routes>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<NavermapScriptComponent />} />
+          <Route path="/gathering" element={<GatheringList />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/detail/:id" element={<DetailedPost />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
