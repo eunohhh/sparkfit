206b9a454a564a727101723521665080077b3875
Sera Choi <c.sera2511@gmail.com>
2024-06-20 21:39:58 +0900

    [style] refector css

--- src/components/myPage/MyPage.jsx
+++ src/components/myPage/MyPage.jsx
@@ -6,21 +6,13 @@ import ClubList from './ClubList';
 const MyPage = () => {
   return (
     <section className="max-w-[1280px] h-[100vh] flex flex-col mt-4 mx-auto">
-      <STSection>
+      <section className="flex flex-col p-2 gap-4 w-full sm:p-1 mx-auto md:p-4 ml-4 lg:ml-6">
         <UserInfo />
         <ClubList />
-      </STSection>
+      </section>
     </section>
   );
 };
-
-// const STMyPageContainer = styled.section`
-//   max-width: 1280px;
-//   height: 100vh;
-//   display: flex;
-//   flex-direction: column;
-//   margin: 1rem auto;
-// `;
 
 export const STSection = styled.section`
   display: flex;
@@ -41,7 +33,7 @@ export const STSection = styled.section`
   }
 
   @media (max-width: 1024px) {
-    margin-left: 2rem;
+    margin-left: 6rem;
   }
 `;
 
